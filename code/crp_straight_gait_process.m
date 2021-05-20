c19b23abf0d492bf4334e25c20f98b93aa88a99b
Cloedp <69530277+Cloedp@users.noreply.github.com>
2021-05-20 14:31:49 +0200

    Update crp_straight_gait_process.m

--- code/crp_straight_gait_process.m
+++ code/crp_straight_gait_process.m
@@ -1,13 +1,23 @@
 %% STEP 1: CONVERT C3D TO ZOO FILE
 
-fld = uigetfolder;       % Select crp_straight_gait
-fld_c3d = [fld,'data/c3d_data'];
-fld1 = [fld,'data/1-c3d2zoo'];
+%fld = uigetfolder;       % Select crp_straight_gait
+%fld_c3d = [fld,'data/c3d_data'];
+%fld1 = [fld,'data/1-c3d2zoo'];
 
 % a) Copy c3d files
-bmech_copyall(fld_c3d,fld1,'all')
+%bmech_copyall(fld_c3d,fld1,'all')
 
-% b) Delete trials that are not self-selected speed 
+% b) Delete abnormal folders (empty)
+subjects = extract_filestruct(fld1);
+for i = 1:length(subjects)
+    fl = engine('fld',fld1, 'search path', subjects{i});
+    if isempty({fl})
+    bmech_removefolder('fld', fld1,'sfld',subjects{i});
+    end
+end
+% c) Delete folder with no mft sheet
+
+% d) Delete trials that are not self-selected speed 
 fldel_gl = engine('fld',fld1,'search file','_gl_'); 
 fldel_gs = engine('fld',fld1,'search file','_gs_'); 
 fldel_st = engine('fld',fld1,'search file','_st_'); 
@@ -15,36 +25,44 @@ delfile(fldel_gl);
 delfile(fldel_gs);
 delfile(fldel_st);
 
-% c) Covert c3d to zoo
+% e) Covert c3d to zoo
 c3d2zoo(fld1,'yes')
-
-%% STEP 2: EXTRACT MUSCLE FUCNTION DATA FROM CSV SHEET
-
-%fld1 = [fld,'data/1-c3d2zoo'];
-%fld2 = [fld,'data/2-muscle_extract'];
-
-% a) Copy step 1 files
-%bmech_copyall(fld1,fld2,'all')
-
-% b) Create new anthro events for channel Anthro 
-bmech_extract_in_mft(fld2,'Age');
-bmech_extract_in_mft(fld2,'Sex');
-bmech_extract_in_mft(fld2,'GMFCS');
       
-%% STEP 3: REMOVE ADULTS (-18 YEARS OLD?)
+%% STEP 2: REMOVE ADULTS (-18 YEARS OLD)
 
-fld2 = [fld,'data/2-muscle_extract'];
-fld3 = [fld,'data/3-remove_adults'];
+fld1 = [fld,'data/1-c3d2zoo'];
+fld2 = [fld,'data/2-remove_adults'];
 
 % a) Copy step 2 files
-bmech_copyall(fld2,fld3,'all')
+bmech_copyall(fld1,fld2,'all')
+
+% b) Extract age of participants 
+bmech_extract_in_mft(fld2,'Age');
 
 % b) Batch remove adults 
-bmech_remove_by_anthro(fld3,'Age',18,'>=');
+bmech_remove_by_anthro(fld2,'Age',18,'>=');
+
+%% STEP 3: EXTRACT MUSCLE FUCNTION DATA FROM CSV SHEET
+
+%fld2 = [fld,'data/2-remove_adults'];
+%fld3 = [fld,'data/3-muscle_extract'];
+
+% a) Copy step 1 files
+%bmech_copyall(fld2,fld3,'all')
+
+% b) Extract Hemiplegia or Diplegia 
+bmech_extract_CP_type(fld3);
+
+% c) Remove Hemi child
+bmech_remove_by_anthro(fld3,'CP_type',hemiplegia,'=');
+
+% d) Extract Anthro Sex & GMFCS  
+bmech_extract_in_mft(fld3,'Sex');
+bmech_extract_in_mft(fld3,'GMFCS');
 
 %% STEP 4: REMOVE UNUSED CHANNELS
 
-fld3 = [fld,'data/3-remove_adults'];
+fld3 = [fld,'data/3-muscle_extract'];
 fld4 = [fld,'data/4-remove_channels'];
 
 % a) Copy step 3 files
