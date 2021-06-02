ba2a0ea167152cf503979dc0963ce98879cd74c7
Cloedp <69530277+Cloedp@users.noreply.github.com>
2021-06-02 14:44:22 +0200

    Update crp_straight_gait_process.m

--- code/crp_straight_gait_process.m
+++ code/crp_straight_gait_process.m
@@ -1,39 +1,47 @@
-%% STEP 1: CONVERT C3D TO ZOO FILE
+%% PREPARATION: ClEANING DATA
 
 fld = uigetfolder;          % Select crp_straight_gait
 fld_c3d = [fld,'data/c3d_data'];
-fld1 = [fld,'data/1-c3d2zoo'];
+fld0 = [fld,'data/0-c3d_clean'];
 
 % a) Copy c3d files
-bmech_copyall(fld_c3d,fld1,'all')
+bmech_copyall(fld_c3d,fld0,'all')
 
 % b) Delete abnormal folders (empty)
-subjects = extract_filestruct(fld1);
+subjects = extract_filestruct(fld0);
 for i = 1:length(subjects)
-    fl = engine('fld',fld1, 'search path', subjects{i}, 'extension','c3d'); % chek for only c3d files
+    fl = engine('fld',fld0, 'search path', subjects{i}, 'extension','c3d'); % chek for only c3d files
     if isempty(fl)                                                          % if no csv files
-    bmech_removefolder(fld1,subjects{i});
+    bmech_removefolder(fld0,subjects{i});
     end
 end
 
 % c) Delete folder with no mft sheet
-subjects = extract_filestruct(fld1);
+subjects = extract_filestruct(fld0);
 for i = 1:length(subjects)
-    fl = engine('fld',fld1, 'search path', subjects{i},'extension','csv');  % chek for only csv files
+    fl = engine('fld',fld0, 'search path', subjects{i},'extension','csv');  % chek for only csv files
     if isempty(fl)                                                          % if no csv file = no mft file
-    bmech_removefolder(fld1,subjects{i});                                   % remove subject because not enough info
+    bmech_removefolder(fld0,subjects{i});                                   % remove subject because not enough info
     end
 end
 
 % d) Delete trials that are not self-selected speed 
-fldel_gl = engine('fld',fld1,'search file','_gl_'); 
-fldel_gs = engine('fld',fld1,'search file','_gs_'); 
-fldel_st = engine('fld',fld1,'search file','_st_'); 
+fldel_gl = engine('fld',fld0,'search file','_gl_'); 
+fldel_gs = engine('fld',fld0,'search file','_gs_'); 
+fldel_st = engine('fld',fld0,'search file','_st_'); 
 delfile(fldel_gl);
 delfile(fldel_gs);
 delfile(fldel_st);
 
-% e) Covert c3d to zoo
+%% STEP 1: CONVERT C3D TO ZOO FILE
+
+fld0 = [fld,'data/0-c3d_clean'];
+fld1 = [fld,'data/1-c3d2zoo'];
+
+% a) Copy c3d clean files
+bmech_copyall(fld0,fld1,'all')
+
+% b) Covert c3d to zoo
 c3d2zoo(fld1,'yes')
       
 %% STEP 2: REMOVE ADULTS (-18 YEARS OLD)
@@ -47,7 +55,7 @@ bmech_copyall(fld1,fld2,'all')
 % b) Extract age of participants 
 bmech_extract_in_mft(fld2,'Age');
 
-% b) Batch remove adults 
+% c) Batch remove adults 
 bmech_remove_by_anthro(fld2,'Age',18,'>=');
 
 %% STEP 3: EXTRACT MUSCLE FUCNTION DATA FROM CSV SHEET
@@ -58,7 +66,7 @@ fld3 = [fld,'data/3-muscle_extract'];
 % a) Copy step 1 files
 bmech_copyall(fld2,fld3,'all')
 
-% d) Extract Anthro Sex & GMFCS  
+% b) Extract Anthro Sex & GMFCS  
 bmech_extract_in_mft(fld3,'Sex');
 bmech_extract_in_mft(fld3,'GMFCS');
 
@@ -72,14 +80,14 @@ bmech_copyall(fld3,fld4,'all')
 
 % b) Remove extra Vicon channels ??NEED MORE FOR THE GPS??
 chkp= {'LHipAngles','RHipAngles', 'LKneeAngles','RKneeAngles',...
-       'LAnkleAngles','RAnkleAngles','SACR','LPelvisAngles','RPelvisAngles',...
-       'LFootProgressAngles','RFootProgressAngles'};  
+       'LAnkleAngles','RAnkleAngles','SACR','LPelvisAngles',...
+       'RPelvisAngles','LFootProgressAngles','RFootProgressAngles'};  
 
 bmech_removechannel('fld',fld4,'chkp','keep')
 
 % c) Explode channels 
 
-bmech_explode (fld4, chkp)
+bmech_explode(fld4,chkp)
 
 %% STEP 5: COMPUTE GPS
 
@@ -98,6 +106,17 @@ fld6 = [fld,'data/6-process_crp'];
 
 % a) Copy step 5 files
 bmech_copyall(fld5,fld6,'all')
+
+% b) Compute CRP for both limb
+bmech_computecrp_straight(fld6,'L')
+bmech_computecrp_straight(fld6,'R')
+
+% c) Calculate deviation phase (DP) and mean absolute relative phase (MARP)
+
+bmech_computeDP_straight(fld6,'L')
+bmech_computeDP_straight(fld6,'R')
+bmech_computeMARP_straight(fld6,'L')
+bmech_computeMARP_straight(fld6,'R')
 
 %% STEP 7: ADD EVENTS
 
