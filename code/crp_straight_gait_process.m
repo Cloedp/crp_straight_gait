7e058c349641f9ec7558111be090aeb45b4a6256
Cloedp <69530277+Cloedp@users.noreply.github.com>
2021-06-10 17:32:53 +0200

    Update crp_straight_gait_process.m
    bug fix

--- code/crp_straight_gait_process.m
+++ code/crp_straight_gait_process.m
@@ -1,6 +1,4 @@
 %% PREPARATION: ClEANING DATA
-% The stucture needs to be a folder (crp_straight_gait) contraining a 'data' folder 
-% with all the participants inside (no more folder separation)
 
 fld = uigetfolder;              % Select crp_straight_gait
 fld_c3d = [fld,filesep, 'data', filesep, 'c3d_data'];
@@ -9,8 +7,16 @@ fld0 = [fld,filesep, 'data', filesep, '0-c3d_clean'];
 % a) Copy c3d files
 copyfile(fld_c3d,fld0);
 
-% b) Delete abnormal folders (empty)
-[~,~,subjects] = extract_filestruct(fld);
+% b) Delete trials that are not self-selected speed 
+fldel_gl = engine('fld',fld0,'search file','_gl_'); 
+fldel_gs = engine('fld',fld0,'search file','_gs_'); 
+fldel_st = engine('fld',fld0,'search file','_st_'); 
+delfile(fldel_gl);
+delfile(fldel_gs);
+delfile(fldel_st);
+
+% c) Delete abnormal folders (empty)
+[~,subjects] = extract_filestruct(fld0);
 for i = 1:length(subjects)
     fl = engine('fld',fld0, 'search path', subjects{i}, 'extension','c3d'); % chek for only c3d files
     if isempty(fl)                                                          % if no csv files
@@ -19,8 +25,8 @@ for i = 1:length(subjects)
     end
 end
 
-% c) Delete folder with no mft sheet
-[~,~,subjects] = extract_filestruct(fld);
+% d) Delete folder with no mft sheet
+[~,subjects] = extract_filestruct(fld0);
 for i = 1:length(subjects)
     fl = engine('fld',fld0, 'search path', subjects{i},'extension','csv');  % chek for only csv files
     if isempty(fl)                                                          % if no csv file = no mft file
@@ -29,19 +35,15 @@ for i = 1:length(subjects)
     end
 end
 
-% d) Delete trials that are not self-selected speed 
-fldel_gl = engine('fld',fld0,'search file','_gl_'); 
-fldel_gs = engine('fld',fld0,'search file','_gs_'); 
-fldel_st = engine('fld',fld0,'search file','_st_'); 
-delfile(fldel_gl);
-delfile(fldel_gs);
-delfile(fldel_st);
-
 %% STEP 1: CONVERT C3D TO ZOO FILE
 
 fld1 = [fld,filesep, 'data', filesep, '1-c3d2zoo'];
 
 % a) Copy c3d clean files
+if exist(fld1, 'dir')
+    disp('removing old  data folder...')
+    rmdir(fld1)
+end
 copyfile(fld0,fld1);
 
 % b) Covert c3d to zoo
@@ -51,7 +53,11 @@ c3d2zoo(fld1,'yes');
 
 fld2 = [fld,filesep, 'data', filesep,'2-remove_adults'];
 
-% a) Copy step 2 files
+% a) Copy step 1 files
+if exist(fld2, 'dir')
+    disp('removing old  data folder...')
+    rmdir(fld2)
+end
 copyfile(fld1,fld2)
 
 % b) Extract age of participants 
@@ -64,21 +70,33 @@ bmech_remove_by_anthro(fld2,'Age',18,'>=');
 
 fld3 = [fld,filesep, 'data', filesep,'3-muscle_extract'];
 
-% a) Copy step 1 files
+% a) Copy step 2 files
+if exist(fld3, 'dir')
+    disp('removing old  data folder...')
+    rmdir(fld3)
+end
 copyfile(fld2,fld3)
 
 % b) Extract Anthro Sex & GMFCS  
 bmech_extract_in_mft(fld3,'Sex');   %(1=M, 2=F)
 bmech_extract_in_mft(fld3,'GMFCS');
 
+% c) delete mft file
+fl = engine('fld',fld3,'extension','csv'); 
+delfile(fl);
+
 %% STEP 4: REMOVE CHANNELS
 
 fld4 = [fld,filesep, 'data', filesep, '4-remove_channels'];
 
 % a) Copy step 3 files
+if exist(fld4, 'dir')
+    disp('removing old  data folder...')
+    rmdir(fld4)
+end
 copyfile(fld3,fld4)
 
-% b) Remove extra Vicon channels ??NEED MORE FOR THE GPS??
+% b) Remove extra Vicon channels
 chkp= {'LHipAngles','RHipAngles', 'LKneeAngles','RKneeAngles',...
        'LAnkleAngles','RAnkleAngles','SACR','LPelvisAngles',...
        'RPelvisAngles','LFootProgressAngles','RFootProgressAngles'};  
@@ -94,16 +112,23 @@ bmech_explode(fld4,chkp)
 fld5 = [fld,filesep, 'data', filesep, '5-compute_gps'];
 
 % a) Copy step 4 files
+if exist(fld5, 'dir')
+    disp('removing old  data folder...')
+    rmdir(fld5)
+end
 copyfile(fld4,fld5);
 
 bmech_gaitprofilescore(fld5);
-
 
 %% STEP 6: CRP PROCESS
 
 fld6 = [fld,filesep, 'data', filesep, '6-process_crp'];
 
 % a) Copy step 5 files
+if exist(fld6, 'dir')
+    disp('removing old  data folder...')
+    rmdir(fld6)
+end
 copyfile(fld5,fld6)
 
 % b) compute phase angle on complete signal
