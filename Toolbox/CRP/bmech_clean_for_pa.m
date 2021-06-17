fab80ed08529a4854f77bedfe864ca10cce68bf4
Cloedp <69530277+Cloedp@users.noreply.github.com>
2021-06-17 17:17:19 +0200

    Update bmech_clean_for_pa.m

--- Toolbox/CRP/bmech_clean_for_pa.m
+++ Toolbox/CRP/bmech_clean_for_pa.m
@@ -1,5 +1,5 @@
 function bmech_clean_for_pa(fld, evt1, evt2)
-% This function remove trials that have missing events and channels beforme
+% This function remove trials that have missing events and channels before
 % computing bmech_phase_angle
 
 cd(fld)
@@ -11,11 +11,14 @@ for i = 1:length(fl)
     data = zload(fl{i});
     
     if isempty(findfield(data, evt1))
-        delfile(fl{i});
+        delete(fl{i});
+        batchdisp(fl{i},'deleting trials because of missing event or channels');
     elseif isempty(findfield(data, evt2))
-        delfile(fl{i});
+        delete(fl{i});
+        batchdisp(fl{i},'deleting trials because of missing event or channels');
     elseif ~isfield(data,'LHipAngles_x')
-        delfile(fl{i});
+        delete(fl{i});
+        batchdisp(fl{i},'deleting trials because of missing event or channels');
     end
 end
 
